a21ee4d4943db8a2cf44ca55bf790ed1bd084ad4
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-10-11 20:44:56 +0900

    # 12865. 평범한 배낭

---
+++ python/baekjoon/12865-2.py
@@ -0,0 +1,8 @@
+m,r=lambda:map(int,input().split()),range
+n,k=m()
+d=[0]*(k+1)
+for _ in r(n):
+    w,v=m()
+    for i in r(k,w-1,-1):
+        d[i]=max(d[i],d[i-w]+v)
+print(d[k])
---
+++ python/baekjoon/12865-3.py
@@ -0,0 +1,11 @@
+# 12854. 평범한 배낭
+
+n, k = map(int, input().split())
+dp = [0] * (k+1)
+
+for i in range(n):
+    w, v = map(int, input().split())
+    for j in range(k, w-1, -1):
+        dp[j] = max(dp[j], dp[j-w] + v)
+
+print(dp[k])
