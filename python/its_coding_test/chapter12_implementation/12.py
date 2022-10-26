c282fe3a2b8e437caf2f95ff39144d005d588b6e
duckbillLvr <86183856+duckbillLvr@users.noreply.github.com>
2022-10-26 16:56:42 +0900

    12. 기둥과 보 설치 (풀이중)

---
+++ python/its_coding_test/chapter12_implementation/12.py
@@ -0,0 +1,17 @@
+# 12. 기둥과 보 설치
+import sys
+
+
+def possible(answer):
+
+def solution(n, build_frame):
+    
+
+input = sys.stdin.readline
+n = int(input())
+build_frame = []
+for i in range(n):
+    x, y, a, b = map(int, input().split())
+    build_frame.append([x, y, a, b])
+
+print(solution(n, build_frame))
