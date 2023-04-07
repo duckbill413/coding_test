9f47defdf3dac5d10f17a6654f00d24a6016f9a2
duckbillLvr <uhyeon7399@daum.net>
2023-04-07 23:22:08 +0900

    # 크루스칼

--- python/doit_coding_test/chapter8-그래프/크루스칼.py
+++ python/doit_coding_test/chapter8-그래프/크루스칼.py
@@ -1,7 +1,10 @@
 # 크루스칼
+# 1197 최소 신장 트리 구하기
 
 import sys
+
 input = sys.stdin.readline
+
 
 def find_parent(parent, x):
     if parent[x] == x:
@@ -10,6 +13,7 @@ def find_parent(parent, x):
         parent[x] = find_parent(parent, parent[x])
         return parent[x]
 
+
 def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
@@ -17,8 +21,9 @@ def union_parent(parent, a, b):
     if a != b:
         parent[b] = a
 
+
 V, E = map(int, input().split())
-parent = [i for i in range(V+1)]
+parent = [i for i in range(V + 1)]
 edges = []
 
 for i in range(E):
