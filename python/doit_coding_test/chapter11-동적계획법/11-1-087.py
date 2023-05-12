728c58ad0e7d729f81fa1501f8efa13766b161d1
duckbill413 <uhyeon7399@daum.net>
2023-05-12 23:15:06 +0900

    # 11726 2*N 타일 채우기

---
+++ python/doit_coding_test/chapter11-동적계획법/11-1-087.py
@@ -0,0 +1,15 @@
+# 11726 2*N 타일 채우기
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+D = [0] * (N+1)
+
+D[0] = 1
+D[1] = 1
+
+for i in range(2, N+1):
+    D[i] = (D[i-1] + D[i-2]) % 10007
+
+print(D[N])
