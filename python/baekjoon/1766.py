a9a03ff7cd8768744b9150a7a06bd92773166121
duckbill413 <uhyeon7399@daum.net>
2023-06-25 01:32:54 +0900

    # 1766 문제집

--- python/baekjoon/1766.py
+++ python/baekjoon/1766.py
@@ -5,8 +5,8 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-indegree = [0] * (N+1)
-graph = [[] for _ in range(N+1)]
+indegree = [0] * (N + 1)
+graph = [[] for _ in range(N + 1)]
 
 for i in range(M):
     a, b = map(int, input().split())
@@ -17,14 +17,14 @@ def topology_sort():
     result = []
     q = []
 
-    for i in range(1, N+1):
+    for i in range(1, N + 1):
         if indegree[i] == 0:
             heapq.heappush(q, i)
 
     while q:
         now = heapq.heappop(q)
         result.append(now)
-c 
+
         for after in graph[now]:
             indegree[after] -= 1
             if indegree[after] == 0:
