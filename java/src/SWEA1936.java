a04a3754ec07e515ae248e243934f439fb50556b
duckbill413 <uhyeon7399@daum.net>
2023-06-28 17:10:03 +0900

    1대1 가위바위보

---
+++ java/src/SWEA1936.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SWEA1936 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        switch (a) {
+            case 1:
+                if (b == 2)
+                    System.out.println('B');
+                else if (b == 3) {
+                    System.out.println('A');
+                }
+                break;
+            case 2:
+                if (b == 1) {
+                    System.out.println('A');
+                } else if (b == 3) {
+                    System.out.println('B');
+                }
+                break;
+            case 3:
+                if (b == 1) {
+                    System.out.println('B');
+                } else if (b == 2) {
+                    System.out.println('A');
+                }
+                break;
+        }
+    }
+}
