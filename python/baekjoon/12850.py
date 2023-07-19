c3f18b13c8811fe7353b97ad6a878e80047dedc9
duckbill413 <uhyeon7399@daum.net>
2023-07-19 17:54:19 +0900

    12850

--- python/baekjoon/12850.py
+++ python/baekjoon/12850.py
@@ -13,7 +13,7 @@ dp = {1: [
     [0, 0, 1, 1, 0, 1, 0, 1],
     [0, 0, 0, 1, 1, 0, 1, 0],
     [0, 0, 0, 0, 0, 1, 0, 1],
-    [0, 0, 0, 0, 1, 1, 0, 0]
+    [0, 0, 0, 0, 1, 0, 1, 0]
 ]}
 
 
@@ -21,19 +21,19 @@ dp = {1: [
 def find_branches(start, end, time):
     # time <= 1 인 경우 왕복이 불가능
     if time <= 1:
-        return dp[1][start][end]
+        return dp[time][start][end]
 
     # dp[time]이 한번도 설정되지 않은 경우 경우의 수를 0으로 초기화
     dp.setdefault(time, [[0] * length for _ in range(length)])
     # 만약 dp[time][start][end]가 0이 아니라면 start -> end 가 이미 한번 고려된 경우이다.
-    if dp[time][start][end] != 0:
+    if dp[time][start][end]:
         return dp[time][start][end]
 
     # 중간 인덱스
     mid = time // 2
-    mid_next = time // 2 if time % 2 == 0 else time // 2 + 1
+    mid_next = mid if time % 2 == 0 else mid + 1
 
-    # 8개 정점에 대해서 start -> end로 가는 중 중간 경로를 나누어서 생각
+    # 8개 정점에 대해서 start -> end 로 가는 중 중간 경로를 나누어서 생각
     for i in range(length):
         dp[time][start][end] += find_branches(start, i, mid) * find_branches(i, end, mid_next)  # 곱연산
         dp[time][start][end] %= MOD
