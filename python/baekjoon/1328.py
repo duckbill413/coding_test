9467e1ef94e0051ead9eebcc2d9ff6fa3c953ace
duckbill413 <uhyeon7399@daum.net>
2023-07-21 01:49:17 +0900

    빌딩순서구하기

--- python/baekjoon/1328.py
+++ python/baekjoon/1328.py
@@ -2,6 +2,16 @@
 import sys
 
 input = sys.stdin.readline
+MOD = 1_000_000_007
 
 N, L, R = map(int, input().split())
 
+dp = [[[0 for k in range(101)] for j in range(101)] for i in range(101)]
+dp[1][1][1] = 1
+
+for k in range(2, N + 1):
+    for i in range(1, L + 1):
+        for j in range(1, R + 1):
+            dp[k][i][j] = (dp[k - 1][i - 1][j] + dp[k - 1][i][j - 1] + (k - 2) * dp[k - 1][i][j]) % MOD
+
+print(dp[N][L][R])
