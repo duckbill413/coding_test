01f6558489729503ad3e292e404e9bcb414e45aa
duckbill413 <uhyeon7399@daum.net>
2023-08-02 17:28:18 +0900

    BOJ15649

---
+++ java/src/baekjoon/BOJ15649.java
@@ -0,0 +1,44 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ15649 {
+    private static int N, M;
+    private static int[] answer;
+    private static boolean[] visited;
+
+    public static void recursion(int num, int length) {
+        answer[length] = num;
+        if (length == M-1) {
+            for (int i=0; i<M; i++) {
+                System.out.print(answer[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i=1; i<=N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                recursion(i, length+1);
+                visited[i] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        answer = new int[M];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            visited[i] = true;
+            recursion(i, 0);
+            visited[i] = false;
+        }
+    }
+}
