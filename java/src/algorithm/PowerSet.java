7fc8d0f2c2307f39d167d17bf675cfae45e9a0d5
duckbill413 <uhyeon7399@daum.net>
2023-08-03 12:21:06 +0900

    부분 집합 Power Set

---
+++ java/src/algorithm/PowerSet.java
@@ -0,0 +1,49 @@
+package algorithm;
+
+// 부분 집합 (Power Set)
+public class PowerSet {
+    private static final int[] arr = {1, 2, 3};
+    private static int N;
+
+    public static void main(String[] args) {
+        N = arr.length;
+        boolean [] visited = new boolean[N];
+
+        System.out.println("Power Set Recursion");
+        powerSet(0, visited);
+        System.out.println("Power Set Bit");
+        bit();
+    }
+
+    private static void bit() {
+        for (int i=0; i< 1 << N; i++){
+            for (int j=0; j<N; j++){
+                if ((i & 1 << j) != 0){
+                    System.out.print(arr[j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private static void powerSet(int idx, boolean[] visited) {
+        if (idx == N){
+            print(visited);
+            return;
+        }
+
+        visited[idx] = false;
+        powerSet(idx+1, visited);
+        visited[idx] = true;
+        powerSet(idx+1, visited);
+    }
+
+    private static void print(boolean[] visited) {
+        for (int i=0; i<N; i++){
+            if (visited[i]) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+}
