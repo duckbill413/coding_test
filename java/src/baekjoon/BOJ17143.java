6c2d4b94d98f3645ec306cc63f84193b4801a0a5
duckbill413 <uhyeon7399@daum.net>
2023-08-22 01:43:11 +0900

    17143 낚시왕

--- java/src/baekjoon/BOJ17143.java
+++ java/src/baekjoon/BOJ17143.java
@@ -2,15 +2,16 @@ package baekjoon;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
+// 17143 낚시왕
 public class BOJ17143 {
+    private static int[] dx = {0, -1, 1, 0, 0};
+    private static int[] dy = {0, 0, 0, 1, -1};
     private static int R, C, M;
     private static Shark[][] sea;
-    private static List<Shark> sharks;
     private static int answer;
 
     public static void main(String[] args) throws Exception {
@@ -22,7 +23,6 @@ public class BOJ17143 {
         M = Integer.parseInt(st.nextToken());
 
         sea = new Shark[R + 1][C + 1];
-        sharks = new ArrayList<>(); // 상어의 크기로 내림차순
 
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -34,77 +34,68 @@ public class BOJ17143 {
 
             Shark shark = new Shark(r, c, s, d, z);
             sea[r][c] = shark;
-            sharks.add(shark);
         }
 
         answer = 0;
         for (int move = 1; move <= C; move++) {
-            moveFisherman(move);
+            moveFisherman(move); // 낚시왕 이동 및 상어 사냥
             moveSharks();
         }
         System.out.println(answer);
     }
 
     private static void moveFisherman(int index) {
-        Shark shark = null;
         for (int i = 1; i <= R; i++) {
             if (sea[i][index] != null) {
-                shark = sea[i][index];
+                answer += sea[i][index].z;
                 sea[i][index] = null;
-                break;
+                return;
             }
-        }
-        if (shark != null) {
-            answer += shark.z;
-            sharks.remove(shark);
         }
     }
 
     private static void moveSharks() {
-        sharks.sort(Comparator.comparingInt(o -> o.z));
-        for (int i = sharks.size() - 1; i >= 0; i--) {
-            Shark shark = sharks.get(i);
-            int r = shark.r;
-            int c = shark.c;
-            if (shark.d == 1 || shark.d == 2) {
-                int tmp = shark.r + shark.s;
-                if (tmp / R != 0 && tmp / R % 2 != 0) {
-                    shark.d = shark.d == 1 ? 2 : 1;
-                }
-
-                if (shark.d == 1) {
-                    shark.r = tmp % R;
-                } else {
-                    shark.r = R - tmp % R;
-                }
-            }
-
-            if (shark.d == 3 || shark.d == 4) {
-                int tmp = shark.c + shark.s;
-                if (tmp / R != 0 && tmp / R % 2 != 0) {
-                    shark.d = shark.d == 3 ? 4 : 3;
-                }
-
-                if (shark.d == 3) {
-                    shark.c = tmp % C;
-                } else {
-                    shark.c = C - tmp % C;
-                }
-            }
-
-            if (sea[shark.r][shark.c] != null) {
-                // 현재 상어의 크기가 넣는 곳 보다 큰 경우
-                if (sea[shark.r][shark.c].z < shark.z) {
-                    sea[shark.r][shark.c] = shark;
-                    sea[r][c] = null;
-                } else {
-                    sharks.remove(shark);
-                }
-            } else {
-                sea[shark.r][shark.c] = shark;
-                sea[r][c] = null;
+        Shark[][] moved = new Shark[R + 1][C + 1];
+        Queue<Shark> sharks = new ArrayDeque<>();
+        // 이동할 상어들을 담는다.
+        for (int i = 0; i < R + 1; i++) {
+            for (int j = 0; j < C + 1; j++) {
+                if (sea[i][j] != null) sharks.add(sea[i][j]);
             }
         }
+
+        // 상어들의 이동
+        // 상어의 크기가 더 크면 잡아 먹기
+        while (!sharks.isEmpty()) {
+            Shark cur = sharks.poll();
+
+            for (int i = 0; i < cur.s; i++) {
+                int nx = cur.r + dx[cur.d];
+                int ny = cur.c + dy[cur.d];
+
+                if (nx < 1 || nx > R || ny < 1 || ny > C) {
+                    if (cur.d == 1) cur.d = 2;
+                    else if (cur.d == 2) cur.d = 1;
+                    else if (cur.d == 3) cur.d = 4;
+                    else if (cur.d == 4) cur.d = 3;
+
+                    nx = cur.r + dx[cur.d];
+                    ny = cur.c + dy[cur.d];
+                }
+                cur.r = nx;
+                cur.c = ny;
+            }
+
+            if (moved[cur.r][cur.c] == null) {
+                moved[cur.r][cur.c] = cur;
+            } else {
+                if (moved[cur.r][cur.c].z < cur.z) {
+                    moved[cur.r][cur.c] = cur;
+                }
+            }
+        }
+
+        sea = moved;
     }
 
     private static class Shark {
