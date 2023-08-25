c7260fe81f9c11c81ac398907bfbb47db38028f9
duckbill413 <uhyeon7399@daum.net>
2023-08-25 15:30:55 +0900

    BOJ10926

---
+++ java/src/baekjoon/BOJ10926.java
@@ -0,0 +1,11 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BOJ10926 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine() + "??!");
+    }
+}
