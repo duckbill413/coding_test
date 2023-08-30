1e357b7f17d82b25faeae24ce59faea46321d001
zini9188 <zqwz@naver.com>
2023-08-30 17:56:53 +0900

    BOJ1010

---
+++ java/src/baekjoon/BOJ1010S2.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1010S2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[31][31];
+
+        for (int i = 0; i < 31; i++) {
+            dp[i][i] = 1;
+            dp[i][1] = i;
+            dp[i][0] = 1;
+        }
+
+        for (int i = 2; i < 31; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+            }
+        }
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(dp[b][a]);
+        }
+    }
+}
