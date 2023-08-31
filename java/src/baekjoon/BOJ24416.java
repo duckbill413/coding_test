1e9543033ffb3df80eda6d590999878287f64dc8
duckbill413 <uhyeon7399@daum.net>
2023-08-31 22:35:42 +0900

    24416 알고리즘 수업 - 피보나치 수 1

---
+++ java/src/baekjoon/BOJ24416.java
@@ -0,0 +1,39 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 24416 알고리즘 수업 - 피보나치 수 1
+public class BOJ24416 {
+    private static int A, B;
+    private static int[] dp;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        A = B = 0;
+        fib1(N);
+
+        dp = new int[N + 1];
+        fib2(N);
+        System.out.println(A + " " + B);
+    }
+
+    private static int fib1(int n) {
+        if (n == 1 || n == 2) {
+            A++;
+            return 1;
+        }
+        return fib1(n - 1) + fib1(n - 2);
+    }
+
+    private static void fib2(int n) {
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            B++;
+        }
+    }
+}
