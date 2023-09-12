cec5bb06c393dcef98c5a952b4e34c9666f7feba
duckbill413 <uhyeon7399@daum.net>
2023-09-12 23:22:14 +0900

    # 햄버거 만들기

---
+++ python/programmers/햄버거 만들기.py
@@ -0,0 +1,11 @@
+# 햄버거 만들기
+def solution(ingredient):
+    answer = 0
+
+    s = []
+    for i in ingredient:
+        s.append(i)
+        if s[-4:] == [1, 2, 3, 1]:
+            answer += 1
+            for i in range(4): s.pop()
+    return answer
