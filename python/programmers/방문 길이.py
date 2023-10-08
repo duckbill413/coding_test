3b1684d1d245c360a0e5fe086cabc78127f6ea8c
duckbill413 <uhyeon7399@daum.net>
2023-10-08 20:14:13 +0900

    방문 개발

---
+++ python/programmers/방문 길이.py
@@ -0,0 +1,28 @@
+move = {
+    'U': (-1, 0),
+    'D': (1, 0),
+    'R': (0, 1),
+    'L': (0, -1)
+}
+
+
+def inRange(x, y):
+    return -5 <= x <= 5 and y >= -5 and y <= 5
+
+
+def solution(dirs):
+    answer = 0
+    visited = set()
+
+    x, y = 0, 0
+    for dir in dirs:
+        nx = x + move[dir][0]
+        ny = y + move[dir][1]
+        if inRange(nx, ny):
+            if (x, y, nx, ny) not in visited and (nx, ny, x, y) not in visited:
+                visited.add((x, y, nx, ny))
+                visited.add((nx, ny, x, y))
+                answer += 1
+            x, y = nx, ny
+
+    return answer
