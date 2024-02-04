44a8603843df6f686a8b64b279d8595cccf81848
duckbill413 <uhyeon7399@daum.net>
2024-02-04 23:40:13 +0900

    [PCCP 기출문제] 9번/이웃한 칸

---
+++ python/programmers/[PCCP 기출문제] 9번/이웃한 칸.py
@@ -0,0 +1,16 @@
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def solution(board, h, w):
+    n = len(board)
+    root = board[h][w]
+
+    answer = 0
+    for i in range(4):
+        nx = h + dx[i]
+        ny = w + dy[i]
+        if 0 <= nx < n and 0 <= ny < n:
+            if board[nx][ny] == root:
+                answer += 1
+    return answer
