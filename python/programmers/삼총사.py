ae71f235d7e9510bc7d711678ca16b7fd7d8cb05
duckbill413 <uhyeon7399@daum.net>
2024-03-28 23:11:46 +0900

    삼총사

---
+++ python/programmers/삼총사.py
@@ -0,0 +1,11 @@
+from itertools import combinations
+
+
+def solution(number):
+    answer = 0
+
+    for case in list(combinations(number, 3)):
+        if sum(case) == 0:
+            answer += 1
+
+    return answer
