2eb3171a8e8d2063e6da44c693d80eabe2be5ee1
duckbill413 <uhyeon7399@daum.net>
2024-04-11 12:54:20 +0900

    코드트리 - 루돌프의 반란

--- python/codetree/루돌프의 반란.py
+++ python/codetree/루돌프의 반란.py
@@ -6,10 +6,13 @@ N, M, P, C, D = map(int, input().split())
 
 rudolph = list(map(int, input().split()))
 
+# idx: x, y, score, status
 santa = {}
+table = [[0 for j in range(N)] for i in range(N)]
 for _ in range(P):
     idx, sr, sc = map(int, input().split())
-    santa[idx] = [sr, sc, 0]  # x, y, score
+    santa[idx] = [sr, sc, 0, True]  # x, y, score
+    table[sr][sc] = idx
 
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
@@ -55,7 +58,32 @@ def move_rudolph(x, y):
     return move
 
 
-def crash(idx, r, d, c):  # 충돌한 산타 idx, 거리, 방향, 부딪힌 주체 (0: 루돌프, 1: 산타)
+def crash(idx, d, c):  # 충돌한 산타 idx, 거리, 방향, 부딪힌 주체 (0: 루돌프, 1: 산타)
     d = (d + 8) % 4  # 이동해온 방향과 반대 방향
     score = C if c == 0 else D
     santa[idx][2] += score  # 산타 점수 증가
+
+    nx = santa[idx][0] + dx[d] * score
+    ny = santa[idx][1] + dy[d] * score
+
+    if not in_range(nx, ny):
+        santa[idx][3] = False
+        return
+
+    while nx == -1 and ny == -1:
+        # 이동하려는 위치에 산타가 없는 경우
+        if table[nx][ny] == 0:
+            table[nx][ny] = idx
+            santa[idx][0] = nx
+            santa[idx][1] = ny
+            nx = ny = -1
+        else:
+            prev_idx = table[nx][ny]
+            # 산타
+            table[nx][ny] = idx
+            santa[idx][0] = nx
+            santa[idx][1] = ny
+            # 기존 산타
+            nx = santa[prev_idx][0] + dx[d]
+            ny = santa[prev_idx][1] + dy[d]
+            santa[prev_idx]
