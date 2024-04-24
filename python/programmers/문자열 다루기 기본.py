b6854b2cd40967ba7fb8d58564c2ea8b9a9b5af5
duckbill413 <uhyeon7399@daum.net>
2024-04-25 00:00:41 +0900

    문자열 다루기 기본

--- python/programmers/문자열 다루기 기본.py
+++ python/programmers/문자열 다루기 기본.py
@@ -1,4 +1,2 @@
 def solution(s):
-    a = list(filter(lambda x: 'A' <= x <= 'z', s))
-
-    return len(s) == 4 or len(s) == 6 and len(list(filter(lambda x: 'A' <= x <= 'z', s))) == 0
+    return (len(s) == 4 or len(s) == 6) and len(list(filter(lambda x: 'A' <= x <= 'z', s))) == 0
