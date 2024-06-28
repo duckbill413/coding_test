74a060f1e92d29e037bbfff93ad7921ebb1fc2d1
duckbill413 <uhyeon7399@daum.net>
2024-06-28 16:33:12 +0900

    12865 평범한 배낭

---
+++ java/src/baekjoon/BOJ12865.java
@@ -0,0 +1,36 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ12865 {
+    private static int N, K; // N: 물품의 수, K: 최대 무게
+    private static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        dp = new int[N + 1][K + 1];
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            for (int j = 1; j < K + 1; j++) {
+                if (w <= j) {
+                    dp[i][j] = Math.max(dp[i - 1][j], v + dp[i - 1][j - w]);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+}
