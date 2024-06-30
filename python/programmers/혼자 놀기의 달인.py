b90a670510bc6cc66de6382487d91e4ada25d8a7
duckbill413 <uhyeon7399@daum.net>
2024-06-30 22:52:29 +0900

    혼자 놀기의 달인

---
+++ python/programmers/혼자 놀기의 달인.py
@@ -0,0 +1,33 @@
+def game(target, group, visited):
+    global N, C
+
+    # 기선택
+    if visited[target - 1]:
+        return
+    # 미선택
+    else:
+        visited[target - 1] = True
+        group.append(target)
+        game(C[target - 1], group, visited)
+
+
+def solution(cards):
+    global N, C
+    N = len(cards)
+    C = cards
+
+    answer = 0
+
+    for i in range(N):
+        visited = [False] * N
+        group1 = []
+        game(C[i], group1, visited)
+        # print("g1: ", group1)
+        for j in range(N):
+            if not visited[C[j] - 1]:
+                group2 = []
+                game(C[j], group2, visited.copy())
+                # print("g2: ", group2)
+                answer = max(answer, len(group1) * len(group2))
+
+    return answer
